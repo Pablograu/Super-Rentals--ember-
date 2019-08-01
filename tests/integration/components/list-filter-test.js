a07214907ad02eaf5d70b361556c7ef74c705a30
Pablograu <paugf89@gmail.com>
2019-08-01 09:13:26 +0200

    added maps

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,25 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rental-listing', function(hooks) {
+module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
-  setupMirage(hooks);
+
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<RentalListing />`);
+    await render(hbs`<ListFilter />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <RentalListing>
+      <ListFilter>
         template block text
-      </RentalListing>
+      </ListFilter>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
--- tests/integration/components/location-map-test.js
+++ tests/integration/components/location-map-test.js
@@ -1,25 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rental-listing', function(hooks) {
+module('Integration | Component | location-map', function(hooks) {
   setupRenderingTest(hooks);
-  setupMirage(hooks);
+
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<RentalListing />`);
+    await render(hbs`<LocationMap />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <RentalListing>
+      <LocationMap>
         template block text
-      </RentalListing>
+      </LocationMap>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
